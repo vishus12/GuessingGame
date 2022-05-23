557a2295706baa2ff8ba93e8ca0d766e2e1fa9da
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-05-23 15:17:21 -0500

    phase 5 completed

--- Program.cs
+++ Program.cs
@@ -1,35 +1,31 @@
 ﻿using System;
 
-namespace guessingGame
+namespace GuessingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey, guess a number!");
+            Random r = new Random();
+            int SecretNum = r.Next(1, 100);
             int index = 0;
             while (index < 4)
             {
-
+                Console.WriteLine("Hey, guess a number!");
                 string response = Console.ReadLine();
                 int ParseResponse = int.Parse(response);
-                int SecretNum = 69;
+
                 if (ParseResponse == SecretNum)
                 {
-                    Console.WriteLine("Success Message!");
+                    Console.WriteLine("Success!");
                 }
                 else
                 {
-                    Console.WriteLine($"Your guess is: {ParseResponse}");
-                    Console.WriteLine($"You have {3 - index} more chances to guess");
+                    Console.WriteLine($"Your guess is {ParseResponse}");
                 }
+                Console.WriteLine($"You have {3 - index} more chances");
                 index++;
             }
         }
-
     }
-
-
 }
-
-

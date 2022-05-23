e6eafd2e63de9b3592dc74203b0ad2cc14d07181
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-05-23 13:41:49 -0500

    Phase 1-2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, guess a number!");
+            string response = Console.ReadLine();
+            int ParseResponse = int.Parse(response);
+            int SecretNum = 69;
+            if (ParseResponse == SecretNum)
+            {
+                Console.WriteLine("Success Message!");
+            }
+            else { Console.WriteLine("Failure Message!"); }
+        }
+    }
+}
+
+
+// The program should be updated to...
+
+// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+// No longer display the user's guess. They know what they guessed, right?
+// Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
